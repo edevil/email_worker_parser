0c3938d537ce134bc1a8a9f3b301badcb1c5ead0
André Cruz <acruz@cloudflare.com>
2022-12-19 14:49:46 +0000

    Use rawSize in order to do fewer allocations

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 const PostalMime = require('postal-mime');
 
-async function streamToArrayBuffer(stream) {
-	let result = new Uint8Array(0);
+async function streamToArrayBuffer(stream, streamSize) {
+	let result = new Uint8Array(streamSize);
+	let bytesRead = 0;
 	const reader = stream.getReader();
 	while (true) {
 		const { done, value } = await reader.read();
 		if (done) {
 			break;
 		}
-
-		const newResult = new Uint8Array(result.length + value.length);
-		newResult.set(result);
-		newResult.set(value, result.length);
-		result = newResult;
+		result.set(value, bytesRead);
+		bytesRead += value.length;
 	}
 	return result;
 }
 
 export default {
 	async email(event, env, ctx) {
-		const rawEmail = await streamToArrayBuffer(event.body);
+		const rawEmail = await streamToArrayBuffer(event.raw, event.rawSize);
 		const parser = new PostalMime.default();
 		const parsedEmail = await parser.parse(rawEmail);
 		console.log("Mail subject: ", parsedEmail.subject);
@@ -30,7 +28,7 @@ export default {
 		} else {
 			parsedEmail.attachments.forEach(att => {
 				console.log("Attachment: ", att.filename);
-				console.log("Attachment dispisition: ", att.disposition);
+				console.log("Attachment disposition: ", att.disposition);
 				console.log("Attachment mime type: ", att.mimeType);
 				console.log("Attachment size: ", att.content.byteLength);
 			});
